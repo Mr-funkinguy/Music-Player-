695325365dac9836d37b055aa4880f9eb37e269a
Mr-funkinguy <89234368+Mr-funkinguy@users.noreply.github.com>
2022-09-01 01:31:29 -0700

    Update script.js

--- Ching Cheng Hanji/script.js
+++ Ching Cheng Hanji/script.js
@@ -6,7 +6,7 @@
 
 const audioPlayer = document.querySelector(".audio-player");
 const audio = new Audio(
-  "Music-Player-/Songs/hanji.mp3"
+  "https://github.com/Mr-funkinguy/Music-Player-/blob/main/Songs/hanji.mp3?raw=true"
 );
 //credit for song: Adrian kreativaweb@gmail.com
 
